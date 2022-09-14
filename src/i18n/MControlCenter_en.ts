ad1f1c19432ec760ddc0c348334ea397dd1c4e4f
Dmitry Serov <dmitri.s93@gmail.com>
2022-09-14 21:15:31 +0300

    Added GUI localization support

---
+++ src/i18n/MControlCenter_en.ts
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>EC Version:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>EC Build:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery charge:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery threshold:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>CPU temp:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>GPU temp:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fan 1:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fan 2:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cooler Boost</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If you mainly use your laptop with adaptor plugget, we suggest you set the charge capacity at a lower percentage (60% or 80%) to prolong your battery lifecycle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Best for Mobility</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Charge the battery to 100% all the time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Balanced</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 70%, stop at 80%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Best for battery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 50%, stop at 60%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Keyboard Backlit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Always on</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Auto turn off in 10 sec</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>FN ⇄ Super</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>WebCam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>USB Power Share</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Version:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>GitHub:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
