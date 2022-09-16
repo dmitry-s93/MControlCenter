bcf504f1c66bd24ac8fbfb713eccb04573a051f0
Dmitry Serov <dmitri.s93@gmail.com>
2022-09-16 17:58:05 +0300

    Added support for Russian language

---
+++ src/i18n/MControlCenter_ru.ts
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Info</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <source>EC Version:</source>
+        <translation>Версия EC:</translation>
+    </message>
+    <message>
+        <source>EC Build:</source>
+        <translation>Сборка EC:</translation>
+    </message>
+    <message>
+        <source>Battery charge:</source>
+        <translation>Заряд батареи:</translation>
+    </message>
+    <message>
+        <source>Battery threshold:</source>
+        <translation>Ограничение заряда:</translation>
+    </message>
+    <message>
+        <source>CPU temp:</source>
+        <translation>Температура ЦП:</translation>
+    </message>
+    <message>
+        <source>GPU temp:</source>
+        <translation>Температура ГП:</translation>
+    </message>
+    <message>
+        <source>Fan 1:</source>
+        <translation>Вентилятор 1:</translation>
+    </message>
+    <message>
+        <source>Fan 2:</source>
+        <translation>Вентилятор 2:</translation>
+    </message>
+    <message>
+        <source>Cooler Boost</source>
+        <translation>Cooler Boost</translation>
+    </message>
+    <message>
+        <source>Battery</source>
+        <translation>Батарея</translation>
+    </message>
+    <message>
+        <source>If you mainly use your laptop with adaptor plugget, we suggest you set the charge capacity at a lower percentage (60% or 80%) to prolong your battery lifecycle</source>
+        <translation>Если вы в основном используете ноутбук с подключенным адаптером питания, мы рекомендуем установить ёмкость заряда на более низкий процент (60% или 80%) для продления срока службы аккумулятора</translation>
+    </message>
+    <message>
+        <source>Best for Mobility</source>
+        <translation>Лучше для мобильности</translation>
+    </message>
+    <message>
+        <source>Charge the battery to 100% all the time</source>
+        <translation>Всегда заряжать батарею до 100%</translation>
+    </message>
+    <message>
+        <source>Balanced</source>
+        <translation>Баланс</translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 70%, stop at 80%</source>
+        <translation>Заряжать батарею, если заряд ниже 70%, остановить на 80%</translation>
+    </message>
+    <message>
+        <source>Best for battery</source>
+        <translation>Лучше для батареи</translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 50%, stop at 60%</source>
+        <translation>Заряжать батарею, если заряд ниже 50%, остановить на 60%</translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation>Другое</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>Keyboard Backlit</source>
+        <translation>Подсветка клавиатуры</translation>
+    </message>
+    <message>
+        <source>Always on</source>
+        <translation>Всегда включена</translation>
+    </message>
+    <message>
+        <source>Auto turn off in 10 sec</source>
+        <translation>Автовыключение через 10 секунд</translation>
+    </message>
+    <message>
+        <source>FN ⇄ Super</source>
+        <translation>FN ⇄ Super</translation>
+    </message>
+    <message>
+        <source>WebCam</source>
+        <translation>Веб-камера</translation>
+    </message>
+    <message>
+        <source>USB Power Share</source>
+        <translation>USB Power Share</translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <translation>Дебаг</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>Version:</source>
+        <translation>Версия:</translation>
+    </message>
+    <message>
+        <source>GitHub:</source>
+        <translation>GitHub:</translation>
+    </message>
+    <message>
+        <source>Charging status:</source>
+        <translation>Статус зарядки:</translation>
+    </message>
+    <message>
+        <source>Charging</source>
+        <translation>Заряжается</translation>
+    </message>
+    <message>
+        <source>Discharging</source>
+        <translation>Разряжается</translation>
+    </message>
+    <message>
+        <source>Not charging</source>
+        <translation>Не заряжается</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Неизвестно</translation>
+    </message>
+</context>
+</TS>
