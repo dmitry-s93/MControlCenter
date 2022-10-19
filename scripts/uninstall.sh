71ed684ad6fe75757d10a2bcc72338ae6e8938f8
Dmitry Serov <dmitri.s93@gmail.com>
2022-10-19 18:25:38 +0300

    Update uninstall.sh

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -10,6 +10,7 @@ rm -fv /home/$SUDO_USER/.local/share/applications/$SHORTCUT
 rm -fv /usr/share/icons/hicolor/scalable/apps/mcontrolcenter.svg
 rm -fv /usr/libexec/mcontrolcenter-helper
 rm -fv /etc/dbus-1/system.d/mcontrolcenter-helper.conf
+rm -fv /usr/share/dbus-1/system.d/mcontrolcenter-helper.conf
 rm -fv /usr/share/dbus-1/system-services/mcontrolcenter.helper.service
 
 echo "Uninstall was successful"
