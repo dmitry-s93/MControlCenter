bf2e16438b76bf1e9166d108f92a95cb42d8ad40
ssergio-ll <sergio@iskynet.es>
2022-10-22 21:20:23 +0200

    Add files via upload

--- src/i18n/MControlCenter_es.ts
+++ src/i18n/MControlCenter_es.ts
@@ -5,199 +5,199 @@
     <name>MainWindow</name>
     <message>
         <source>Info</source>
-        <translation type="unfinished"></translation>
+        <translation>Info</translation>
     </message>
     <message>
         <source>EC Version:</source>
-        <translation type="unfinished"></translation>
+        <translation>EC Version:</translation>
     </message>
     <message>
         <source>EC Build:</source>
-        <translation type="unfinished"></translation>
+        <translation>EC Build:</translation>
     </message>
     <message>
-        <source>Carga de la batería:</source>
-        <translation type="unfinished"></translation>
+        <source>Battery charge:</source>
+        <translation>Carga de la batería:</translation>
     </message>
     <message>
-        <source>Límite de carga de batería:</source>
-        <translation type="unfinished"></translation>
+        <source>Battery threshold:</source>
+        <translation>Límite de carga de batería:</translation>
     </message>
     <message>
-        <source>Temperatura CPU:</source>
-        <translation type="unfinished"></translation>
+        <source>CPU temp:</source>
+        <translation>Temperatura CPU:</translation>
     </message>
     <message>
-        <source>Temperatura GPU:</source>
-        <translation type="unfinished"></translation>
+        <source>GPU temp:</source>
+        <translation>Temperatura GPU:</translation>
     </message>
     <message>
-        <source>Ventilador 1:</source>
-        <translation type="unfinished"></translation>
+        <source>Fan 1:</source>
+        <translation>Ventilador 1:</translation>
     </message>
     <message>
-        <source>Ventilador 2:</source>
-        <translation type="unfinished"></translation>
+        <source>Fan 2:</source>
+        <translation>Ventilador 2:</translation>
     </message>
     <message>
         <source>Cooler Boost</source>
-        <translation type="unfinished"></translation>
+        <translation>Cooler Boost</translation>
     </message>
     <message>
-        <source>Batería</source>
-        <translation type="unfinished"></translation>
+        <source>Battery</source>
+        <translation>Batería</translation>
     </message>
     <message>
-        <source>Si sueles usar tu portátil normalmente con el adaptador, te recomendamos establezcas la capacidad de carga con un porcentaje más bajo (60% o 80%) para prolongar la vida de tu batería.</source>
-        <translation type="unfinished"></translation>
+        <source>If you mainly use your laptop with adaptor plugged, we suggest you set the charge capacity at a lower percentage (60% or 80%) to prolong your battery lifecycle</source>
+        <translation>Si sueles usar tu portátil normalmente con el adaptador, te recomendamos establezcas la capacidad de carga con un porcentaje más bajo (60% o 80%) para prolongar la vida de tu batería</translation>
     </message>
     <message>
-        <source>Mejor para la mobilidad</source>
-        <translation type="unfinished"></translation>
+        <source>Best for Mobility</source>
+        <translation>Mejor para la mobilidad</translation>
     </message>
     <message>
-        <source>Carga la batería hasta el 100% todo el tiempo</source>
-        <translation type="unfinished"></translation>
+        <source>Charge the battery to 100% all the time</source>
+        <translation>Carga la batería hasta el 100% todo el tiempo</translation>
     </message>
     <message>
-        <source>Balanceado</source>
-        <translation type="unfinished"></translation>
+        <source>Balanced</source>
+        <translation>Balanceado</translation>
     </message>
     <message>
-        <source>Carga la batería cuando esta por debajo del 70% hasta llegar al 80%</source>
-        <translation type="unfinished"></translation>
+        <source>Charge the battery when under 70%, stop at 80%</source>
+        <translation>Carga la batería cuando esta por debajo del 70% hasta llegar al 80%</translation>
     </message>
     <message>
-        <source>Lo mejor para la batería</source>
-        <translation type="unfinished"></translation>
+        <source>Best for battery</source>
+        <translation>Lo mejor para la batería</translation>
     </message>
     <message>
-        <source>Carga la batería cuando esta por debajo del 50% hasta llegar al 60%</source>
-        <translation type="unfinished"></translation>
+        <source>Charge the battery when under 50%, stop at 60%</source>
+        <translation>Carga la batería cuando esta por debajo del 50% hasta llegar al 60%</translation>
     </message>
     <message>
-        <source>Personalizado</source>
-        <translation type="unfinished"></translation>
+        <source>Custom</source>
+        <translation>Personalizado</translation>
     </message>
     <message>
-        <source>Ajustes</source>
-        <translation type="unfinished"></translation>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
     </message>
     <message>
-        <source>Teclado retroiluminado</source>
-        <translation type="unfinished"></translation>
+        <source>Keyboard Backlit</source>
+        <translation>Teclado retroiluminado</translation>
     </message>
     <message>
-        <source>Siempre encendido</source>
-        <translation type="unfinished"></translation>
+        <source>Always on</source>
+        <translation>Siempre encendido</translation>
     </message>
     <message>
-        <source>Apagar automáticamente en 10 sec</source>
-        <translation type="unfinished"></translation>
+        <source>Auto turn off in 10 sec</source>
+        <translation>Apagar automáticamente en 10 sec</translation>
     </message>
     <message>
         <source>FN ⇄ Super</source>
-        <translation type="unfinished"></translation>
+        <translation>FN ⇄ Super</translation>
     </message>
     <message>
         <source>WebCam</source>
-        <translation type="unfinished"></translation>
+        <translation>WebCam</translation>
     </message>
     <message>
         <source>USB Power Share</source>
-        <translation type="unfinished"></translation>
+        <translation>USB Power Share</translation>
     </message>
     <message>
         <source>Debug</source>
-        <translation type="unfinished"></translation>
+        <translation>Debug</translation>
     </message>
     <message>
-        <source>Acerca de</source>
-        <translation type="unfinished"></translation>
+        <source>About</source>
+        <translation>Acerca de</translation>
     </message>
     <message>
-        <source>Versión:</source>
-        <translation type="unfinished"></translation>
+        <source>Version:</source>
+        <translation>Versión:</translation>
     </message>
     <message>
         <source>GitHub:</source>
-        <translation type="unfinished"></translation>
+        <translation>GitHub:</translation>
     </message>
     <message>
-        <source>Estado de la carga:</source>
-        <translation type="unfinished"></translation>
+        <source>Charging status:</source>
+        <translation>Estado de la carga:</translation>
     </message>
     <message>
-        <source>Cargando</source>
-        <translation type="unfinished"></translation>
+        <source>Charging</source>
+        <translation>Cargando</translation>
     </message>
     <message>
-        <source>Descargando</source>
-        <translation type="unfinished"></translation>
+        <source>Discharging</source>
+        <translation>Descargando</translation>
     </message>
     <message>
-        <source>No carga</source>
-        <translation type="unfinished"></translation>
+        <source>Not charging</source>
+        <translation>No carga</translation>
     </message>
     <message>
-        <source>Desconocido</source>
-        <translation type="unfinished"></translation>
+        <source>Unknown</source>
+        <translation>Desconocido</translation>
     </message>
     <message>
-        <source>Seguimiento de Bugs:</source>
-        <translation type="unfinished"></translation>
+        <source>Bug tracker:</source>
+        <translation>Seguimiento de Bugs:</translation>
     </message>
     <message>
-        <source>Una aplicación que te permite cambiar los ajustes de tus portátiles en MSI en Linux</source>
-        <translation type="unfinished"></translation>
+        <source>An application that allows you to change the settings of MSI laptops running Linux</source>
+        <translation>Una aplicación que te permite cambiar los ajustes de tus portátiles en MSI en Linux</translation>
     </message>
     <message>
-        <source>Intercambiar botones FN y Super</source>
-        <translation type="unfinished"></translation>
+        <source>Swap FN and Super buttons</source>
+        <translation>Intercambiar botones FN y Super</translation>
     </message>
     <message>
-        <source>Totalmente cargada</source>
-        <translation type="unfinished"></translation>
+        <source>Fully charged</source>
+        <translation>Totalmente cargada</translation>
     </message>
     <message>
-        <source>Totalmente cargada (Descargando)</source>
-        <translation type="unfinished"></translation>
+        <source>Fully charged (Discharging)</source>
+        <translation>Totalmente cargada (Descargando)</translation>
     </message>
     <message>
         <source>rpm</source>
-        <translation type="unfinished"></translation>
+        <translation>rpm</translation>
     </message>
     <message>
-        <source>Modo</source>
-        <translation type="unfinished"></translation>
+        <source>Mode</source>
+        <translation>Modo</translation>
     </message>
     <message>
-        <source>Alto rendimiento</source>
-        <translation type="unfinished"></translation>
+        <source>High Performance</source>
+        <translation>Alto rendimiento</translation>
     </message>
     <message>
-        <source>Brinda de un equilibrio entre rendimiento y el cuidado de vida de la batería</source>
-        <translation type="unfinished"></translation>
+        <source>A mode that brings a balance between performance and battery live</source>
+        <translation>Brinda un equilibrio entre rendimiento y el cuidado de vida de la batería</translation>
     </message>
     <message>
-        <source>Silencio</source>
-        <translation type="unfinished"></translation>
+        <source>Silent</source>
+        <translation>Silencioso</translation>
     </message>
     <message>
-        <source>Super Batería</source>
-        <translation type="unfinished"></translation>
+        <source>Super Battery</source>
+        <translation>Super Batería</translation>
     </message>
     <message>
-        <source>Provee del mejor rendimiento para multi-tareas y trabajos pesados</source>
-        <translation type="unfinished"></translation>
+        <source>A mode that provides the best system performance for multi-tasking and heavy duty</source>
+        <translation>Provee del mejor rendimiento para multi-tareas y trabajos pesados</translation>
     </message>
     <message>
-        <source>Modo silencioso que reduce el ruido de los ventiladores</source>
-        <translation type="unfinished"></translation>
+        <source>A quiet mode that lowers the fan noise</source>
+        <translation>Modo silencioso que reduce el ruido de los ventiladores</translation>
     </message>
     <message>
-        <source>Modo de ahorro de energía que reduce el consumo de energía para tareas básicas</source>
-        <translation type="unfinished"></translation>
+        <source>A power-saving mode that lowers the power consumption for basic needs</source>
+        <translation>Modo de ahorro de energía que reduce el consumo de energía para tareas básicas</translation>
     </message>
 </context>
 </TS>
