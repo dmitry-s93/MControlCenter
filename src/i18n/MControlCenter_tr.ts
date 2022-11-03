247e2124d266e0cb66de586073733bcb545597b8
emrgncr <egencer20@ku.edu.tr>
2022-11-03 17:45:11 +0300

    Turkish translation (#57)
    * Turkish language translation
    
    * turkish translation
    
    * Copied source for untranslated lines and marked complate
    
    * Remove comment on "Always on"
    
    I guess my initial translation fits that context.

---
+++ src/i18n/MControlCenter_tr.ts
@@ -0,0 +1,229 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="tr_TR" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Info</source>
+        <translation>Bilgi</translation>
+    </message>
+    <message>
+        <source>EC Version:</source>
+        <translatorcomment>don&apos;t know how to translate this</translatorcomment>
+        <translation>EC Version:</translation>
+    </message>
+    <message>
+        <source>EC Build:</source>
+        <translatorcomment>don&apos;t know how to translate this</translatorcomment>
+        <translation>EC Build:</translation>
+    </message>
+    <message>
+        <source>Battery charge:</source>
+        <translation>Pil şarjı:</translation>
+    </message>
+    <message>
+        <source>Battery threshold:</source>
+        <translation>Pil sınırı:</translation>
+    </message>
+    <message>
+        <source>CPU temp:</source>
+        <translation>CPU-sıcaklığı:</translation>
+    </message>
+    <message>
+        <source>GPU temp:</source>
+        <translation>GPU sıcaklığı:</translation>
+    </message>
+    <message>
+        <source>Fan 1:</source>
+        <translation>Fan 1:</translation>
+    </message>
+    <message>
+        <source>Fan 2:</source>
+        <translation>Fan 2:</translation>
+    </message>
+    <message>
+        <source>Cooler Boost</source>
+        <translation>Cooler Boost</translation>
+    </message>
+    <message>
+        <source>Battery</source>
+        <translation>Pil</translation>
+    </message>
+    <message>
+        <source>If you mainly use your laptop with adaptor plugged, we suggest you set the charge capacity at a lower percentage (60% or 80%) to prolong your battery lifecycle</source>
+        <translation>Eğer laptopunuzu genelde adaptöre takılı kullanıyorsanız pilinizin ömrünü uzatmak için şarj kapasitesini düşük bir değere (%60 - %80) ayarlamanızı öneriyoruz</translation>
+    </message>
+    <message>
+        <source>Best for Mobility</source>
+        <translation>Hareket için en iyi</translation>
+    </message>
+    <message>
+        <source>Charge the battery to 100% all the time</source>
+        <translation>Pili her zaman dolana kadar şarj et</translation>
+    </message>
+    <message>
+        <source>Balanced</source>
+        <translation>Dengeli</translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 70%, stop at 80%</source>
+        <translation>Pili %70&apos;in altındayken şarj et, %80&apos;e ulaşınca dur</translation>
+    </message>
+    <message>
+        <source>Best for battery</source>
+        <translation>Pil ömrü için en iyi</translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 50%, stop at 60%</source>
+        <translation>Pili %50&apos;nin altındayken şarj et, %60&apos;a ulaşınca dur</translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation>Özel</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ayarlar</translation>
+    </message>
+    <message>
+        <source>Keyboard Backlit</source>
+        <translation>Klavye Işığı</translation>
+    </message>
+    <message>
+        <source>Always on</source>
+        <translation>Hep açık</translation>
+    </message>
+    <message>
+        <source>Auto turn off in 10 sec</source>
+        <translation>10 saniye sonra otomatik kapat</translation>
+    </message>
+    <message>
+        <source>FN ⇄ Super</source>
+        <translation>FN ⇄ Super</translation>
+    </message>
+    <message>
+        <source>WebCam</source>
+        <translation>Web Kamerası</translation>
+    </message>
+    <message>
+        <source>USB Power Share</source>
+        <translation>USB Güç Paylaşımı</translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <translation>Hata ayıkla</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Hakkında</translation>
+    </message>
+    <message>
+        <source>Version:</source>
+        <translation>Versiyon:</translation>
+    </message>
+    <message>
+        <source>GitHub:</source>
+        <translation>GitHub:</translation>
+    </message>
+    <message>
+        <source>Charging status:</source>
+        <translation>Pil durumu:</translation>
+    </message>
+    <message>
+        <source>Charging</source>
+        <translation>Doluyor</translation>
+    </message>
+    <message>
+        <source>Discharging</source>
+        <translation>Boşalıyor</translation>
+    </message>
+    <message>
+        <source>Not charging</source>
+        <translation>Dolmuyor</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Bilinmiyor</translation>
+    </message>
+    <message>
+        <source>Bug tracker:</source>
+        <translatorcomment>not sure on this one</translatorcomment>
+        <translation>Hata takibi:</translation>
+    </message>
+    <message>
+        <source>An application that allows you to change the settings of MSI laptops running Linux</source>
+        <translation>Linux çalıştıran MSI laptopların ayarlarını değiştirmenizi sağlayan bir program</translation>
+    </message>
+    <message>
+        <source>Swap FN and Super buttons</source>
+        <translation>FN ve Super butonlarını değiştir</translation>
+    </message>
+    <message>
+        <source>Fully charged</source>
+        <translation>Tamamen dolu</translation>
+    </message>
+    <message>
+        <source>Fully charged (Discharging)</source>
+        <translation>Tamemen dolu (Boşalıyor)</translation>
+    </message>
+    <message>
+        <source>rpm</source>
+        <translation>rpm</translation>
+    </message>
+    <message>
+        <source>Mode</source>
+        <translation>Mod</translation>
+    </message>
+    <message>
+        <source>High Performance</source>
+        <translation>Yüksek Performans</translation>
+    </message>
+    <message>
+        <source>A mode that brings a balance between performance and battery live</source>
+        <translation>Performans ile pil ömrünü dengeleyen bir mod</translation>
+    </message>
+    <message>
+        <source>Silent</source>
+        <translation>Sessiz</translation>
+    </message>
+    <message>
+        <source>Super Battery</source>
+        <translatorcomment>I don&apos;t know what this is</translatorcomment>
+        <translation>Super Battery</translation>
+    </message>
+    <message>
+        <source>A mode that provides the best system performance for multi-tasking and heavy duty</source>
+        <translatorcomment>how to translate multi-tasking?</translatorcomment>
+        <translation>Aynı anda birden çok görev çalıştıran ve ağır yük taşıyan sistemler için en iyi performansı sağlayan mod</translation>
+    </message>
+    <message>
+        <source>A quiet mode that lowers the fan noise</source>
+        <translation>Fan gürültüsünü azaltan bir sessiz mod</translation>
+    </message>
+    <message>
+        <source>A power-saving mode that lowers the power consumption for basic needs</source>
+        <translation>Temel kullanım için güç kullanımını azaltarak güç tasarrufu sağlayan bir mod</translation>
+    </message>
+    <message>
+        <source>Failed to load the ec_sys kernel module</source>
+        <translatorcomment>leaving this as is since it will be easier to find a solution online if the error is in English</translatorcomment>
+        <translation>Failed to load the ec_sys kernel module</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Uygula</translation>
+    </message>
+    <message>
+        <source>Show</source>
+        <translation>Göster</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Çıkış</translation>
+    </message>
+    <message>
+        <source>Charge limit</source>
+        <translation>Şarj limiti</translation>
+    </message>
+</context>
+</TS>
