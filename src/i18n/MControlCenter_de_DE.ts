642d61a6dac9e8fcffcdbbf824e75d7c45012572
EchterAlsFake <EchterAlsFake@proton.me>
2023-03-19 00:04:59 +0100

    Added German translations :)

---
+++ src/i18n/MControlCenter_de_DE.ts
@@ -0,0 +1,267 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <source>EC Version:</source>
+        <translation>EC Version:</translation>
+    </message>
+    <message>
+        <source>EC Build:</source>
+        <translation>EC Build:</translation>
+    </message>
+    <message>
+        <source>Battery charge:</source>
+        <translation>Batterie Ladung:</translation>
+    </message>
+    <message>
+        <source>Battery threshold:</source>
+        <translation>Batterie Schwelle:</translation>
+    </message>
+    <message>
+        <source>CPU temp:</source>
+        <translation>CPU Temperatur:</translation>
+    </message>
+    <message>
+        <source>GPU temp:</source>
+        <translation>GPU Temperatur:</translation>
+    </message>
+    <message>
+        <source>Fan 1:</source>
+        <translation>Lüfter 1:</translation>
+    </message>
+    <message>
+        <source>Fan 2:</source>
+        <translation>Lüfter 2:</translation>
+    </message>
+    <message>
+        <source>Cooler Boost</source>
+        <translation>Cooler Boost</translation>
+    </message>
+    <message>
+        <source>Battery</source>
+        <translation>Batterie</translation>
+    </message>
+    <message>
+        <source>If you mainly use your laptop with adaptor plugged, we suggest you set the charge capacity at a lower percentage (60% or 80%) to prolong your battery lifecycle</source>
+        <translation>Falls sie ihren Laptop hauptsächlich mit Ladekabel benutzen, empfehlen wir ihnen die Ladekapazität niedriger zu setzen (60% oder 80%) um die Lebensdauer zu erhöhen</translation>
+    </message>
+    <message>
+        <source>Best for Mobility</source>
+        <translation>Am besten für Mobilität</translation>
+    </message>
+    <message>
+        <source>Charge the battery to 100% all the time</source>
+        <translation>Aufladen der Batterie auf 100% die ganze Zeit</translation>
+    </message>
+    <message>
+        <source>Balanced</source>
+        <translation>Ausgeglichen</translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 70%, stop at 80%</source>
+        <translation>Lade die Batterie wenn sie unter 70% ist, und stoppe das Laden bei 80%</translation>
+    </message>
+    <message>
+        <source>Best for battery</source>
+        <translation>Am besten für die Batterie</translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 50%, stop at 60%</source>
+        <translation>Lade die Batterie, wenn unter 50% und stoppe das Laden bei 60%</translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation>Custom</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <source>Keyboard Backlit</source>
+        <translation>Tastatur Beleuchtung</translation>
+    </message>
+    <message>
+        <source>Always on</source>
+        <translation>Immer angeschaltet</translation>
+    </message>
+    <message>
+        <source>Auto turn off in 10 sec</source>
+        <translation>Automatisch in 10 Sekunden ausschalten</translation>
+    </message>
+    <message>
+        <source>FN ⇄ Super</source>
+        <translation>FN ⇄ Super</translation>
+    </message>
+    <message>
+        <source>WebCam</source>
+        <translation>Kamera</translation>
+    </message>
+    <message>
+        <source>USB Power Share</source>
+        <translation>USB Power Share</translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <translation>Debug</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <source>Version:</source>
+        <translation>Version:</translation>
+    </message>
+    <message>
+        <source>GitHub:</source>
+        <translation>GitHub:</translation>
+    </message>
+    <message>
+        <source>Charging status:</source>
+        <translation>Lade Status:</translation>
+    </message>
+    <message>
+        <source>Charging</source>
+        <translation>Laden</translation>
+    </message>
+    <message>
+        <source>Discharging</source>
+        <translation>Entladen</translation>
+    </message>
+    <message>
+        <source>Not charging</source>
+        <translation>Lädt nicht</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Unbekannt</translation>
+    </message>
+    <message>
+        <source>Bug tracker:</source>
+        <translation>Fehler Tracker:</translation>
+    </message>
+    <message>
+        <source>An application that allows you to change the settings of MSI laptops running Linux</source>
+        <translation>Eine Anwendung, welche ihnen das Ändern der Einstellungen für MSI Laptops unter Linux erlaubt</translation>
+    </message>
+    <message>
+        <source>Swap FN and Super buttons</source>
+        <translation>FN und Super Tasten tauschen</translation>
+    </message>
+    <message>
+        <source>Fully charged</source>
+        <translation>Vollständig geladen</translation>
+    </message>
+    <message>
+        <source>Fully charged (Discharging)</source>
+        <translation>Vollständig geladen (Entlädt)</translation>
+    </message>
+    <message>
+        <source>rpm</source>
+        <translation>rpm</translation>
+    </message>
+    <message>
+        <source>Mode</source>
+        <translation>Modus</translation>
+    </message>
+    <message>
+        <source>High Performance</source>
+        <translation>Hohe Leistung</translation>
+    </message>
+    <message>
+        <source>A mode that brings a balance between performance and battery live</source>
+        <translation>Ein Modus, welcher eine gute Balance zwischen der Leistung und der Batterielebensdauer bringt</translation>
+    </message>
+    <message>
+        <source>Silent</source>
+        <translation>Leise</translation>
+    </message>
+    <message>
+        <source>Super Battery</source>
+        <translation>Exteme Batterie Leistung</translation>
+    </message>
+    <message>
+        <source>A mode that provides the best system performance for multi-tasking and heavy duty</source>
+        <translation>Ein Modus, welcher die beste System Leistung für anspruchsvolle Aufgaben mit sich bringt</translation>
+    </message>
+    <message>
+        <source>A quiet mode that lowers the fan noise</source>
+        <translation>Ein leiser Modus, welcher die Lüfterlautstärke reduziert</translation>
+    </message>
+    <message>
+        <source>A power-saving mode that lowers the power consumption for basic needs</source>
+        <translation>Ein Energiesparmodus, der den Stromverbrauch für Grundbedürfnisse senkt</translation>
+    </message>
+    <message>
+        <source>Failed to load the ec_sys kernel module</source>
+        <translation>Das ec_sys Kernel Modul konnte nicht geladen werden</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Anwenden</translation>
+    </message>
+    <message>
+        <source>Show</source>
+        <translation>Anzeigen</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <source>Charge limit</source>
+        <translation>Lade Limit</translation>
+    </message>
+    <message>
+        <source>Fan Mode:</source>
+        <translation>Lüfter Modus:</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation>Automatisch</translation>
+    </message>
+    <message>
+        <source>Basic</source>
+        <translation>Basic</translation>
+    </message>
+    <message>
+        <source>Advanced</source>
+        <translation>Erweitert</translation>
+    </message>
+    <message>
+        <source>Choose the mode that best suits your use case</source>
+        <translation>Wähle den Modus, welcher am besten für dein Nutzungsverhalten geeignet ist</translation>
+    </message>
+    <message>
+        <source>Cooling</source>
+        <translation>Kühlen</translation>
+    </message>
+    <message>
+        <source>Fan 1 speed</source>
+        <translation>Geschwindigkeit des 1. Lüfters</translation>
+    </message>
+    <message>
+        <source>Fan 2 speed</source>
+        <translation>Geschwindigkeit des 2. Lüfters</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Zurücksetzen</translation>
+    </message>
+    <message>
+        <source>Fan control</source>
+        <translation>Lüftersteuerung</translation>
+    </message>
+    <message>
+        <source>Enable advanced fan control</source>
+        <translation>Aktiviere die erweiterte Lüftersteuerung</translation>
+    </message>
+</context>
+</TS>
