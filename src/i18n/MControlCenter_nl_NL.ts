fd3f8f45668feb0baa460ac11fe79bce40635416
Mika <mika@cashew.network>
2024-05-01 23:38:00 +0200

    added dutch translations

---
+++ src/i18n/MControlCenter_nl_NL.ts
@@ -0,0 +1,267 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Info</source>
+        <translation>Informatie</translation>
+    </message>
+    <message>
+        <source>EC Version:</source>
+        <translation>EC Versie:</translation>
+    </message>
+    <message>
+        <source>EC Build:</source>
+        <translation>EC Build:</translation>
+    </message>
+    <message>
+        <source>Battery charge:</source>
+        <translation>Acculading:</translation>
+    </message>
+    <message>
+        <source>Battery threshold:</source>
+        <translation>Drempel Accu:</translation>
+    </message>
+    <message>
+        <source>CPU temp:</source>
+        <translation>CPU temperatuur:</translation>
+    </message>
+    <message>
+        <source>GPU temp:</source>
+        <translation>GPU temperatuur:</translation>
+    </message>
+    <message>
+        <source>Fan 1:</source>
+        <translation>Ventilator 1:</translation>
+    </message>
+    <message>
+        <source>Fan 2:</source>
+        <translation>Ventilator 2:</translation>
+    </message>
+    <message>
+        <source>Cooler Boost</source>
+        <translation>Cooler Boost</translation>
+    </message>
+    <message>
+        <source>Battery</source>
+        <translation>Accu</translation>
+    </message>
+    <message>
+        <source>If you mainly use your laptop with adaptor plugged, we suggest you set the charge capacity at a lower percentage (60% or 80%) to prolong your battery lifecycle</source>
+        <translation>Als je je laptop vooral gebruikt wanneer deze aangesloten is op het stroomnet, suggereren we dat je de acculading stopt op een laag percentage (60% tot 80%) om de batterijduur te verlengen.</translation>
+    </message>
+    <message>
+        <source>Best for Mobility</source>
+        <translation>Beste voor mobiliteit</translation>
+    </message>
+    <message>
+        <source>Charge the battery to 100% all the time</source>
+        <translation>Laad de accu altijd tot 100%</translation>
+    </message>
+    <message>
+        <source>Balanced</source>
+        <translation>Gebalanceerd</translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 70%, stop at 80%</source>
+        <translation>Laad de accu als deze onder 70% is, stop bij 80%</translation>
+    </message>
+    <message>
+        <source>Best for battery</source>
+        <translation>Beste voor de accu</translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 50%, stop at 60%</source>
+        <translation>Laad de accu wanneer deze onder de 50% is, stop bij 60%</translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation>Aangepast</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <source>Keyboard Backlit</source>
+        <translation>Toetsenbordverlichting</translation>
+    </message>
+    <message>
+        <source>Always on</source>
+        <translation>Altijd aan</translation>
+    </message>
+    <message>
+        <source>Auto turn off in 10 sec</source>
+        <translation>Automatisch in 10 seconden uit</translation>
+    </message>
+    <message>
+        <source>FN ⇄ Super</source>
+        <translation>FN ⇄ Super</translation>
+    </message>
+    <message>
+        <source>WebCam</source>
+        <translation>Camera</translation>
+    </message>
+    <message>
+        <source>USB Power Share</source>
+        <translation>USB Power Share</translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <translation>Debug</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Over applicatie</translation>
+    </message>
+    <message>
+        <source>Version:</source>
+        <translation>Versie:</translation>
+    </message>
+    <message>
+        <source>GitHub:</source>
+        <translation>GitHub:</translation>
+    </message>
+    <message>
+        <source>Charging status:</source>
+        <translation>Oplaad Status:</translation>
+    </message>
+    <message>
+        <source>Charging</source>
+        <translation>Opladen</translation>
+    </message>
+    <message>
+        <source>Discharging</source>
+        <translation>Ontladen</translation>
+    </message>
+    <message>
+        <source>Not charging</source>
+        <translation>Laad niet op</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Onbekend</translation>
+    </message>
+    <message>
+        <source>Bug tracker:</source>
+        <translation>Bug tracker:</translation>
+    </message>
+    <message>
+        <source>An application that allows you to change the settings of MSI laptops running Linux</source>
+        <translation>Een applicatie die het toelaat om de instellingen van een MSI laptop te veranderen die Linux draait</translation>
+    </message>
+    <message>
+        <source>Swap FN and Super buttons</source>
+        <translation>Verwissel de FN en Super knoppen</translation>
+    </message>
+    <message>
+        <source>Fully charged</source>
+        <translation>Volledig geladen</translation>
+    </message>
+    <message>
+        <source>Fully charged (Discharging)</source>
+        <translation>Volledig geladen (Aan het ontladen)</translation>
+    </message>
+    <message>
+        <source>rpm</source>
+        <translation>rpm</translation>
+    </message>
+    <message>
+        <source>Mode</source>
+        <translation>Mode</translation>
+    </message>
+    <message>
+        <source>High Performance</source>
+        <translation>Hoge Prestaties</translation>
+    </message>
+    <message>
+        <source>A mode that brings a balance between performance and battery live</source>
+        <translation>Een modus die balans tussen prestaties en batterijleven geeft</translation>
+    </message>
+    <message>
+        <source>Silent</source>
+        <translation>Stil</translation>
+    </message>
+    <message>
+        <source>Super Battery</source>
+        <translation>Extreme accu modus</translation>
+    </message>
+    <message>
+        <source>A mode that provides the best system performance for multi-tasking and heavy duty</source>
+        <translation>Een modus dat de beste prestaties voor multitasking en zware taken geeft</translation>
+    </message>
+    <message>
+        <source>A quiet mode that lowers the fan noise</source>
+        <translation>Een stille modus dat ventilatorgeluid verminderd</translation>
+    </message>
+    <message>
+        <source>A power-saving mode that lowers the power consumption for basic needs</source>
+        <translation>Een stroombesparende modus dat de stroomconsumptie verminderd</translation>
+    </message>
+    <message>
+        <source>Failed to load the ec_sys kernel module</source>
+        <translation>De ec_sys module\ kon niet geladen worden</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Toepassen</translation>
+    </message>
+    <message>
+        <source>Show</source>
+        <translation>Toon</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Afsluiten</translation>
+    </message>
+    <message>
+        <source>Charge limit</source>
+        <translation>Oplaadlimiet</translation>
+    </message>
+    <message>
+        <source>Fan Mode:</source>
+        <translation>Ventilatormodus:</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation>Automatisch</translation>
+    </message>
+    <message>
+        <source>Basic</source>
+        <translation>Basis</translation>
+    </message>
+    <message>
+        <source>Advanced</source>
+        <translation>Geavanceerd</translation>
+    </message>
+    <message>
+        <source>Choose the mode that best suits your use case</source>
+        <translation>Kies de modus die het beste uitkomt voor jouw gebruik</translation>
+    </message>
+    <message>
+        <source>Cooling</source>
+        <translation>Koelen</translation>
+    </message>
+    <message>
+        <source>Fan 1 speed</source>
+        <translation>Snelheid ventilator 1</translation>
+    </message>
+    <message>
+        <source>Fan 2 speed</source>
+        <translation>Snelheid ventilator 2</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Reset</translation>
+    </message>
+    <message>
+        <source>Fan control</source>
+        <translation>Ventilator instellingen</translation>
+    </message>
+    <message>
+        <source>Enable advanced fan control</source>
+        <translation>Activeer geavanceerde ventilatorinstellingen</translation>
+    </message>
+</context>
+</TS>
