feadd9e0202a3f80c4bf2434a78ba5c4783dbd4d
Nguyen Hoang Ky <nhktmdzhg@gmail.com>
2025-01-24 20:32:24 +0700

    add translation for Vietnamese (vi_VN)

---
+++ src/i18n/MControlCenter_vi.ts
@@ -0,0 +1,275 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="vi_VN" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>EC Version:</source>
+        <translation>Phiên bản EC</translation>
+    </message>
+    <message>
+        <source>EC Build:</source>
+        <translation>Bản dựng EC</translation>
+    </message>
+    <message>
+        <source>Battery charge:</source>
+        <translation>Sạc pin</translation>
+    </message>
+    <message>
+        <source>Battery threshold:</source>
+        <translation>Ngưỡng pin</translation>
+    </message>
+    <message>
+        <source>CPU temp:</source>
+        <translation>Nhiệt độ CPU</translation>
+    </message>
+    <message>
+        <source>GPU temp:</source>
+        <translation>Nhiệt độ GPU</translation>
+    </message>
+    <message>
+        <source>Cooler Boost</source>
+        <translation>Cooler Boost</translation>
+    </message>
+    <message>
+        <source>Battery</source>
+        <translation>Pin</translation>
+    </message>
+    <message>
+        <source>Best for Mobility</source>
+        <translation>Tốt nhất cho di động</translation>
+    </message>
+    <message>
+        <source>Charge the battery to 100% all the time</source>
+        <translation>Luôn sạc pin đến 100%</translation>
+    </message>
+    <message>
+        <source>Balanced</source>
+        <translation>Cân bằng</translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 70%, stop at 80%</source>
+        <translation>Sạc khi pin dưới 70%, dừng khi đạt 80%</translation>
+    </message>
+    <message>
+        <source>Best for battery</source>
+        <translation>Tốt nhất cho pin</translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 50%, stop at 60%</source>
+        <translation>Sạc khi pin dưới 50%, dừng khi đạt 60%</translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation>Tùy chỉnh</translation>
+    </message>
+    <message>
+        <source>Keyboard Backlit</source>
+        <translation>Đèn nền bàn phím</translation>
+    </message>
+    <message>
+        <source>Always on</source>
+        <translation>Luôn bật</translation>
+    </message>
+    <message>
+        <source>Auto turn off in 10 sec</source>
+        <translation>Tự động tắt sau 10 giây</translation>
+    </message>
+    <message>
+        <source>WebCam</source>
+        <translation>Webcam</translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <translation>Sửa lỗi</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Về</translation>
+    </message>
+    <message>
+        <source>Version:</source>
+        <translation>Phiên bản:</translation>
+    </message>
+    <message>
+        <source>Charging status:</source>
+        <translation>Trạng thái sạc:</translation>
+    </message>
+    <message>
+        <source>Charging</source>
+        <translation>Đang sạc</translation>
+    </message>
+    <message>
+        <source>Discharging</source>
+        <translation>Rút sạc</translation>
+    </message>
+    <message>
+        <source>Not charging</source>
+        <translation>Không sạc</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Không rõ</translation>
+    </message>
+    <message>
+        <source>Swap FN and Super buttons</source>
+        <translation>Chuyển đổi giữa nút Fn và Super (Windows)</translation>
+    </message>
+    <message>
+        <source>Fully charged</source>
+        <translation>Đã sạc đầy</translation>
+    </message>
+    <message>
+        <source>Fully charged (Discharging)</source>
+        <translation>Đã sạc đầy (ngắt sạc)</translation>
+    </message>
+    <message>
+        <source>rpm</source>
+        <translation>vòng/phút</translation>
+    </message>
+    <message>
+        <source>Mode</source>
+        <translation>Chế độ</translation>
+    </message>
+    <message>
+        <source>Silent</source>
+        <translation>Im lặng</translation>
+    </message>
+    <message>
+        <source>Super Battery</source>
+        <translation>Siêu tiết kiệm pin</translation>
+    </message>
+    <message>
+        <source>Failed to load the ec_sys kernel module</source>
+        <translation>Thất bại khi tải module kernel ec_sys</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Áp dụng</translation>
+    </message>
+    <message>
+        <source>Show</source>
+        <translation>Hiển thị</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Thoát</translation>
+    </message>
+    <message>
+        <source>Charge limit</source>
+        <translation>Giới hạn sạc</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation>Tự động</translation>
+    </message>
+    <message>
+        <source>Basic</source>
+        <translation>Cơ bản</translation>
+    </message>
+    <message>
+        <source>Advanced</source>
+        <translation>Nâng cao</translation>
+    </message>
+    <message>
+        <source>Cooling</source>
+        <translation>Tản nhiệt</translation>
+    </message>
+    <message>
+        <source>Fan 1 speed</source>
+        <translation>Tốc độ quạt 1</translation>
+    </message>
+    <message>
+        <source>Fan 2 speed</source>
+        <translation>Tốc độ quạt 2</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Cài đặt lại</translation>
+    </message>
+    <message>
+        <source>Fan control</source>
+        <translation>Điều khiển quạt</translation>
+    </message>
+    <message>
+        <source>Enable advanced fan control</source>
+        <translation>Bật điều khiển quạt nâng cao</translation>
+    </message>
+    <message>
+        <source>Overview</source>
+        <translation>Tổng quan</translation>
+    </message>
+    <message>
+        <source>&amp;High Performance</source>
+        <translation>&amp;Hiệu năng cao</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Maximum performance at the cost of heat and increased power consumption&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Hiệu năng tối đa với nhiệt độ và năng lượng tiêu thụ tăng&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>The middle spot between fan noise and power usage</source>
+        <translation>Cân bằng giữa tiếng ổn quạt và năng lượng sử dụng</translation>
+    </message>
+    <message>
+        <source>Low fan noise and moderate power usage</source>
+        <translation>Tiếng ổn quạt thấp và năng lượng sử dụng vừa phải</translation>
+    </message>
+    <message>
+        <source>Limits performance and turns off fans at lower temperatures</source>
+        <translation>Giới hạn cấu hình và tắt quạt khi nhiệt độ thấp</translation>
+    </message>
+    <message>
+        <source>GPU Fan:</source>
+        <translation>Quạt GPU:</translation>
+    </message>
+    <message>
+        <source>CPU Fan:</source>
+        <translation>Quạt CPU:</translation>
+    </message>
+    <message>
+        <source>USB Power</source>
+        <translation>Nguồn điện USB</translation>
+    </message>
+    <message>
+        <source>FN ⇄ Meta</source>
+        <translation>FN ⇄ Meta</translation>
+    </message>
+    <message>
+        <source>Current fan Mode:</source>
+        <translation>Chế độ quạt hiện tại</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If you mainly use your laptop with the charger plugged most of the time, it is recommended to set the charge capacity at a lower percentage (60% or 80%) to prolong your battery lifecycle.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Nếu bạn chủ yếu dùng laptop cùng sạc, thì khuyến khích bạn cài đặt công suất sạc ở mức thấp (60% hoặc 80%) để kéo dài vòng đời của pin của bạn.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Keyboard</source>
+        <translation>Bàn phím</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;MC&lt;/span&gt;ontrol&lt;span style=&quot; font-weight:700;&quot;&gt;C&lt;/span&gt;enter (MCC) is an application that allows you to change the settings of MSI laptops running Linux.&lt;/p&gt;&lt;p&gt;MCC acts as a graphical interface for the &lt;span style=&quot; font-weight:700;&quot;&gt;MSI-EC &lt;/span&gt;driver that already exist in the Linux kernel, if your device is not supported (grey buttons/limited in-app functionality), please visit the msi-ec github page to get help.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;MC&lt;/span&gt;ontrol&lt;span style=&quot; font-weight:700;&quot;&gt;C&lt;/span&gt;enter (MCC) là một ứng dụng cho phép bạn thay đổi cài đặt của máy tính xách tay MSI chạy Linux.&lt;/p&gt;&lt;p&gt;MCC hoạt động như một giao diện đồ họa cho &lt;span style=&quot; font-weight:700;&quot;&gt;MSI-EC &lt;/span&gt;driver đã tồn tại trong kernel Linux, nếu thiết bị của bạn không được hỗ trợ (các nút màu xám/chức năng trong ứng dụng bị giới hạn), vui lòng truy cập trang github msi-ec để được trợ giúp.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>MCC GitHub:</source>
+        <translation>MCC Github:</translation>
+    </message>
+    <message>
+        <source>MCC Bug tracker:</source>
+        <translation>MCC theo dõi bug:</translation>
+    </message>
+    <message>
+        <source>MSI-EC GitHub:</source>
+        <translation>MSI-EC Github:</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/BeardOverflow/msi-ec&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#1ec974;&quot;&gt;https://github.com/BeardOverflow/msi-ec&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/BeardOverflow/msi-ec&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#1ec974;&quot;&gt;https://github.com/BeardOverflow/msi-ec&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Warning&lt;/span&gt;: Writing the wrong values to the wrong addresses &lt;span style=&quot; font-weight:700;&quot;&gt;WILL BRICK YOUR DEVICE!&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Never write to EC memory without knowing how to do a proper BIOS/EC reset, keep in mind that a reset &lt;span style=&quot; font-weight:700;&quot;&gt;might not&lt;/span&gt; fix the device if the device got bricked/broken. &lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Cảnh báo&lt;/span&gt;: Viết sai giá trị vào sai địa chỉ &lt;span style=&quot; font-weight:700;&quot;&gt;SẼ BIẾN THIẾT BỊ CỦA BẠN THÀNH CỤC GẠCH!&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Không bao giờ viết vào bộ nhớ EC mà không hiểu cách thiết đặt lại BIOS/EC, hãy nhớ rằng thiết lập lại &lt;span style=&quot; font-weight:700;&quot;&gt;có thể không&lt;/span&gt; sửa thiết bị cũa bạn nếu thiết bị của bạn thành cục gạch &lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+</TS>
