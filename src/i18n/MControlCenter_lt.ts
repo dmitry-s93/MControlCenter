b1719e87a8136618fb4b6e1fa4c5ef6e72b3fcd7
Dryapanda <dryapanda@proton.me>
2025-02-12 10:37:00 +0200

    Adding Lithuanian language

---
+++ src/i18n/MControlCenter_lt.ts
@@ -0,0 +1,275 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt_lt" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>EC Version:</source>
+        <translation type="EC Versija:"></translation>
+    </message>
+    <message>
+        <source>EC Build:</source>
+        <translation type="EC Versija:"></translation>
+    </message>
+    <message>
+        <source>Battery charge:</source>
+        <translation type="Baterijos įkrova:"></translation>
+    </message>
+    <message>
+        <source>Battery threshold:</source>
+        <translation type="Baterijos slenksčio būklė"></translation>
+    </message>
+    <message>
+        <source>CPU temp:</source>
+        <translation type="CPU temperatūra"></translation>
+    </message>
+    <message>
+        <source>GPU temp:</source>
+        <translation type="GPU temperatūra"></translation>
+    </message>
+    <message>
+        <source>Cooler Boost</source>
+        <translation type="Aušinimo Boost"></translation>
+    </message>
+    <message>
+        <source>Battery</source>
+        <translation type="Baterija"></translation>
+    </message>
+    <message>
+        <source>Best for Mobility</source>
+        <translation type="Geriausia mobilumui"></translation>
+    </message>
+    <message>
+        <source>Charge the battery to 100% all the time</source>
+        <translation type="Baterija visada įkraunama iki 100%"></translation>
+    </message>
+    <message>
+        <source>Balanced</source>
+        <translation type="Subalansuotas"></translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 70%, stop at 80%</source>
+        <translation type="Įkrauna bateriją kai mažiau nei 70%, sustoja kai 80%"></translation>
+    </message>
+    <message>
+        <source>Best for battery</source>
+        <translation type="Geriausia baterijai"></translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 50%, stop at 60%</source>
+        <translation type="Įkrauna bateriją kai mažiau nei 50%, sustoja kai 80%"></translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation type="Tinkintas"></translation>
+    </message>
+    <message>
+        <source>Keyboard Backlit</source>
+        <translation type="Klaviatūros apšvietimas"></translation>
+    </message>
+    <message>
+        <source>Always on</source>
+        <translation type="Visada įjungtas"></translation>
+    </message>
+    <message>
+        <source>Auto turn off in 10 sec</source>
+        <translation type="Automatiškai išsijungia po 10 sekundžių"></translation>
+    </message>
+    <message>
+        <source>WebCam</source>
+        <translation type="Vaizdo kamera"></translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <translation type="Debugavimas"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="Apie"></translation>
+    </message>
+    <message>
+        <source>Version:</source>
+        <translation type="Versija:"></translation>
+    </message>
+    <message>
+        <source>Charging status:</source>
+        <translation type="Įkrovos būsena"></translation>
+    </message>
+    <message>
+        <source>Charging</source>
+        <translation type="Kraunasi"></translation>
+    </message>
+    <message>
+        <source>Discharging</source>
+        <translation type="Išsikrauna"></translation>
+    </message>
+    <message>
+        <source>Not charging</source>
+        <translation type="Nesikrauna"></translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation type="Nežinoma"></translation>
+    </message>
+    <message>
+        <source>Swap FN and Super buttons</source>
+        <translation type="Pakeisti FN ir Super mygtukus"></translation>
+    </message>
+    <message>
+        <source>Fully charged</source>
+        <translation type="Pilnai įkrautas"></translation>
+    </message>
+    <message>
+        <source>Fully charged (Discharging)</source>
+        <translation type="Pilnai įkrautas (Išsikrauna)"></translation>
+    </message>
+    <message>
+        <source>rpm</source>
+        <translation type="apm"></translation>
+    </message>
+    <message>
+        <source>Mode</source>
+        <translation type="Režimas"></translation>
+    </message>
+    <message>
+        <source>Silent</source>
+        <translation type="Tylus"></translation>
+    </message>
+    <message>
+        <source>Super Battery</source>
+        <translation type="Tausojantis bateriją"></translation>
+    </message>
+    <message>
+        <source>Failed to load the ec_sys kernel module</source>
+        <translation type="Nepavyko užkrauti ec_sys branduolio modulio"></translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation type="Pritaikyti"></translation>
+    </message>
+    <message>
+        <source>Show</source>
+        <translation type="Rodyti"></translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation type="Išeiti"></translation>
+    </message>
+    <message>
+        <source>Charge limit</source>
+        <translation type="Įkrovos limitas"></translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation type="Automatiškai"></translation>
+    </message>
+    <message>
+        <source>Basic</source>
+        <translation type="Paprastas"></translation>
+    </message>
+    <message>
+        <source>Advanced</source>
+        <translation type="Pažangus"></translation>
+    </message>
+    <message>
+        <source>Cooling</source>
+        <translation type="Aušinimas"></translation>
+    </message>
+    <message>
+        <source>Fan 1 speed</source>
+        <translation type="1 Ventiliatoriaus greitis"></translation>
+    </message>
+    <message>
+        <source>Fan 2 speed</source>
+        <translation type="2 Ventiliatoriaus greitis"></translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation type="Atkūrti"></translation>
+    </message>
+    <message>
+        <source>Fan control</source>
+        <translation type="Ventiliatoriaus kontrolė"></translation>
+    </message>
+    <message>
+        <source>Enable advanced fan control</source>
+        <translation type="Įjungti pažangią ventiliatoriaus kontrolę"></translation>
+    </message>
+    <message>
+        <source>Overview</source>
+        <translation type="Apžvalga"></translation>
+    </message>
+    <message>
+        <source>&amp;High Performance</source>
+        <translation type="Aukščiausias našumas"></translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Maximum performance at the cost of heat and increased power consumption&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="Maksimalus veikimas reikalaujantis daugiau elektros ir aukštesnės šilumos"></translation>
+    </message>
+    <message>
+        <source>The middle spot between fan noise and power usage</source>
+        <translation type="Vidurinė pozicija tarp ventiliatoriaus triukšmo ir elektros suvartojimo"></translation>
+    </message>
+    <message>
+        <source>Low fan noise and moderate power usage</source>
+        <translation type="Mažas ventiliatoriaus triukšmas ir vidutinis elektros suvartojimas"></translation>
+    </message>
+    <message>
+        <source>Limits performance and turns off fans at lower temperatures</source>
+        <translation type="Riboja našumą ir išjungia ventiliatorius esant žemesnėms temperatūroms"></translation>
+    </message>
+    <message>
+        <source>GPU Fan:</source>
+        <translation type="GPU ventiliatorius"></translation>
+    </message>
+    <message>
+        <source>CPU Fan:</source>
+        <translation type="CPU ventiliatorius"></translation>
+    </message>
+    <message>
+        <source>USB Power</source>
+        <translation type="USB maitinimas"></translation>
+    </message>
+    <message>
+        <source>FN ⇄ Meta</source>
+        <translation type="FN ⇄ Meta"></translation>
+    </message>
+    <message>
+        <source>Current fan Mode:</source>
+        <translation type="Dabartinis ventiliatoriaus režimas"></translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If you mainly use your laptop with the charger plugged most of the time, it is recommended to set the charge capacity at a lower percentage (60% or 80%) to prolong your battery lifecycle.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="Jeigu dažniausiai naudojate savo nešiojamąjį kompiuterį įkroviklio, esant įjungtam į elektrą, rekomenduojama nustatyti įkrovimo talpą mažesne procentine dalimi (60% arba 80%) siekiant ilgesnės baterijos gyvavimo trukmės."></translation>
+    </message>
+    <message>
+        <source>Keyboard</source>
+        <translation type="Klaviatūra"></translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;MC&lt;/span&gt;ontrol&lt;span style=&quot; font-weight:700;&quot;&gt;C&lt;/span&gt;enter (MCC) is an application that allows you to change the settings of MSI laptops running Linux.&lt;/p&gt;&lt;p&gt;MCC acts as a graphical interface for the &lt;span style=&quot; font-weight:700;&quot;&gt;MSI-EC &lt;/span&gt;driver that already exist in the Linux kernel, if your device is not supported (grey buttons/limited in-app functionality), please visit the msi-ec github page to get help.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="(MCC) yra programa, leidžianti keisti MSI nešiojamųjų kompiuterių, veikiančių Linux, nustatymus. MCC veikia kaip grafinė sąsaja MSI-EC tvarkyklėms, kurios jau yra įtrauktos į Linux branduolį, jei jūsų įrenginys nėra palaikomas (pilki mygtukai/ribota programos funkcionalumas), prašome apsilankyti msi-ec github puslapyje, kad gautumėte pagalbą."></translation>
+    </message>
+    <message>
+        <source>MCC GitHub:</source>
+        <translation type="MCC GitHub puslapis"></translation>
+    </message>
+    <message>
+        <source>MCC Bug tracker:</source>
+        <translation type="MCC Klaidų sekiklis"></translation>
+    </message>
+    <message>
+        <source>MSI-EC GitHub:</source>
+        <translation type="MSI-EC GitHub puslapis"></translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/BeardOverflow/msi-ec&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#1ec974;&quot;&gt;https://github.com/BeardOverflow/msi-ec&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="https://github.com/BeardOverflow/msi-ec"></translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Warning&lt;/span&gt;: Writing the wrong values to the wrong addresses &lt;span style=&quot; font-weight:700;&quot;&gt;WILL BRICK YOUR DEVICE!&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Never write to EC memory without knowing how to do a proper BIOS/EC reset, keep in mind that a reset &lt;span style=&quot; font-weight:700;&quot;&gt;might not&lt;/span&gt; fix the device if the device got bricked/broken. &lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="ĮSPĖJIMAS Rašant neteisingas reikšmes į neteisingus adresus GALI SUNAIKINTI JŪSŲ ĮRENGINĮ! Niekuomet neirašykite į EC atmintį be žinojimo, kaip atlikti tinkamą BIOS/EC atstatymą, turėkite omenyje, kad atstatymas gali neatsitaisyti įrenginio, jei įrenginys buvo sunaikintas/sugadintas."></translation>
+    </message>
+</context>
+</TS>
