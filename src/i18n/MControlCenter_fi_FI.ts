0b2e3437eaa43b3a5ec75df3687e104d6f9debe0
WinSer2003 <93113929+WinSer2003@users.noreply.github.com>
2025-06-18 15:51:08 +0300

    Create MControlCenter_fi_FI.ts

---
+++ src/i18n/MControlCenter_fi_FI.ts
@@ -0,0 +1,275 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>EC Version:</source>
+        <translation>EC Versio:</translation>
+    </message>
+    <message>
+        <source>EC Build:</source>
+        <translation>EC Käännös:</translation>
+    </message>
+    <message>
+        <source>Battery charge:</source>
+        <translation>Akun taso:</translation>
+    </message>
+    <message>
+        <source>Battery threshold:</source>
+        <translation>Akun Latausraja:</translation>
+    </message>
+    <message>
+        <source>CPU temp:</source>
+        <translation>Prosessorin lämpötila:</translation>
+    </message>
+    <message>
+        <source>GPU temp:</source>
+        <translation>Näytönohjaimen lämpötila:</translation>
+    </message>
+    <message>
+        <source>Cooler Boost</source>
+        <translation>Jäähdyttimen tehotila</translation>
+    </message>
+    <message>
+        <source>Battery</source>
+        <translation>Akku</translation>
+    </message>
+    <message>
+        <source>Best for Mobility</source>
+        <translation>Paras kannettavuuteen</translation>
+    </message>
+    <message>
+        <source>Charge the battery to 100% all the time</source>
+        <translation>Lataa akku täyteen aina</translation>
+    </message>
+    <message>
+        <source>Balanced</source>
+        <translation>Tasapainotettu</translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 70%, stop at 80%</source>
+        <translation>Lataa akkua kun varaustaso on alle 70% lopeta lataaminen 80%</translation>
+    </message>
+    <message>
+        <source>Best for battery</source>
+        <translation>Paras akulle</translation>
+    </message>
+    <message>
+        <source>Charge the battery when under 50%, stop at 60%</source>
+        <translation>Lataa akkua kun varaustaso on alle 50, lopeta lataaminen 60%</translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation>Muokattu</translation>
+    </message>
+    <message>
+        <source>Keyboard Backlit</source>
+        <translation>Näppäimmistön takavalo</translation>
+    </message>
+    <message>
+        <source>Always on</source>
+        <translation>Aina päällä</translation>
+    </message>
+    <message>
+        <source>Auto turn off in 10 sec</source>
+        <translation>Sammuta automaattisesti 10 sekunnissa</translation>
+    </message>
+    <message>
+        <source>WebCam</source>
+        <translation>Webkamera</translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <translation>Debuggaus</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Tietoa</translation>
+    </message>
+    <message>
+        <source>Version:</source>
+        <translation>Versio:</translation>
+    </message>
+    <message>
+        <source>Charging status:</source>
+        <translation>Lataustila: </translation>
+    </message>
+    <message>
+        <source>Charging</source>
+        <translation>Lataa</translation>
+    </message>
+    <message>
+        <source>Discharging</source>
+        <translation>Tyhjenee</translation>
+    </message>
+    <message>
+        <source>Not charging</source>
+        <translation>Ei lataa</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Tuntematon</translation>
+    </message>
+    <message>
+        <source>Swap FN and Super buttons</source>
+        <translation>Vaihda FN ja super painikkeet</translation>
+    </message>
+    <message>
+        <source>Fully charged</source>
+        <translation>Täyteen ladattu</translation>
+    </message>
+    <message>
+        <source>Fully charged (Discharging)</source>
+        <translation>Täyteen ladattu(tyhjenee)</translation>
+    </message>
+    <message>
+        <source>rpm</source>
+        <translation>rpm</translation>
+    </message>
+    <message>
+        <source>Mode</source>
+        <translation>Tila</translation>
+    </message>
+    <message>
+        <source>Silent</source>
+        <translation>Hiljainen</translation>
+    </message>
+    <message>
+        <source>Super Battery</source>
+        <translation>Super-akku</translation>
+    </message>
+    <message>
+        <source>Failed to load the ec_sys kernel module</source>
+        <translation>Virhe ladatessa ec_sys kernel moduulia</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Käytä</translation>
+    </message>
+    <message>
+        <source>Show</source>
+        <translation>Näytä</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Poistu</translation>
+    </message>
+    <message>
+        <source>Charge limit</source>
+        <translation>Latausraja</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation>Automaattinen</translation>
+    </message>
+    <message>
+        <source>Basic</source>
+        <translation>Perus</translation>
+    </message>
+    <message>
+        <source>Advanced</source>
+        <translation>Edistynyt</translation>
+    </message>
+    <message>
+        <source>Cooling</source>
+        <translation>Jäähdytys</translation>
+    </message>
+    <message>
+        <source>Fan 1 speed</source>
+        <translation>Tuulettimen 1 nopeus</translation>
+    </message>
+    <message>
+        <source>Fan 2 speed</source>
+        <translation>Tuulettimen 2 nopeus</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Nollaa</translation>
+    </message>
+    <message>
+        <source>Fan control</source>
+        <translation>Tuulettimen ohjaus</translation>
+    </message>
+    <message>
+        <source>Enable advanced fan control</source>
+        <translation>Aktivoi edistynyt tuulettimen ohjaus</translation>
+    </message>
+    <message>
+        <source>Overview</source>
+        <translation>Yleisnäkymä</translation>
+    </message>
+    <message>
+        <source>&amp;High Performance</source>
+        <translation>&amp; Korkea teho</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Maximum performance at the cost of heat and increased power consumption&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt; Maksimi teho lämpötilan ja sähkön kulutuksen hinnalla&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>The middle spot between fan noise and power usage</source>
+        <translation>Keskikohta tuulettimen äänessä ja sähkön kulutuksessa</translation>
+    </message>
+    <message>
+        <source>Low fan noise and moderate power usage</source>
+        <translation>Matala tuulettimen ääni ja keskitasoinen sähkön käyttö</translation>
+    </message>
+    <message>
+        <source>Limits performance and turns off fans at lower temperatures</source>
+        <translation>Rajoittaa tehon ja laittaa tuulettimet pois matalilla lämpötiloilla</translation>
+    </message>
+    <message>
+        <source>GPU Fan:</source>
+        <translation>Näytönohjaimen tuuletin:</translation>
+    </message>
+    <message>
+        <source>CPU Fan:</source>
+        <translation>Prosessorin tuuletin:</translation>
+    </message>
+    <message>
+        <source>USB Power</source>
+        <translation>USB virta</translation>
+    </message>
+    <message>
+        <source>FN ⇄ Meta</source>
+        <translation>FN ⇄ Meta </translation>
+    </message>
+    <message>
+        <source>Current fan Mode:</source>
+        <translation>Aktiivinen tuulettimen tila: </translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If you mainly use your laptop with the charger plugged most of the time, it is recommended to set the charge capacity at a lower percentage (60% or 80%) to prolong your battery lifecycle.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Jos käytät pääasiassa laitettasi laturi kiinni on suositeltua asettaa akun prosentti 60 tai 80 prosenttiin.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Keyboard</source>
+        <translation>Näppäimmistö</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;MC&lt;/span&gt;ontrol&lt;span style=&quot; font-weight:700;&quot;&gt;C&lt;/span&gt;enter (MCC) is an application that allows you to change the settings of MSI laptops running Linux.&lt;/p&gt;&lt;p&gt;MCC acts as a graphical interface for the &lt;span style=&quot; font-weight:700;&quot;&gt;MSI-EC &lt;/span&gt;driver that already exist in the Linux kernel, if your device is not supported (grey buttons/limited in-app functionality), please visit the msi-ec github page to get help.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MCC GitHub:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MCC Bug tracker:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MSI-EC GitHub:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/BeardOverflow/msi-ec&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#1ec974;&quot;&gt;https://github.com/BeardOverflow/msi-ec&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Warning&lt;/span&gt;: Writing the wrong values to the wrong addresses &lt;span style=&quot; font-weight:700;&quot;&gt;WILL BRICK YOUR DEVICE!&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Never write to EC memory without knowing how to do a proper BIOS/EC reset, keep in mind that a reset &lt;span style=&quot; font-weight:700;&quot;&gt;might not&lt;/span&gt; fix the device if the device got bricked/broken. &lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
